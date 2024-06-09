fd647b5cc4b127025703c6c9bf67fa7e77101133
Jared Johnston <jared.johnston@me.com>
2024-06-09 23:19:43 +0100

    Fixed indenting error in conditionals.py

--- intro_to_programming/flow_control/conditionals.py
+++ intro_to_programming/flow_control/conditionals.py
@@ -2,10 +2,9 @@ value = int(input("Enter a number: "))
 
 if value == 3:
   print("value is 3")
-
 else:
-  if value == 4:
-  print("value is 4")
+    if value == 4:
+    print("value is 4")
 
-  else:
-  print("value is not 3 or 4")
+    else:
+    print("value is not 3 or 4")
