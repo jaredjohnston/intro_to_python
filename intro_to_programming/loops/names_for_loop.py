db7aca123a4766cfc4a1c00736406542ab4d4dda
Jared Johnston <jared.johnston@me.com>
2024-06-18 13:23:46 +0100

    Created names_for_loop.py file

---
+++ intro_to_programming/loops/names_for_loop.py
@@ -0,0 +1,8 @@
+names = ['Jared', 'Daniel', 'Eugene', 'Beatrice']
+upper_names = []
+
+for name in names:
+  upper_name = names[index].upper()
+  upper_names.append(upper_name)
+
+print(upper_names)
