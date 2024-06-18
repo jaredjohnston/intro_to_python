a7772bb75b0db5bd7d0dd65f6288e7f681dd87ee
Jared Johnston <jared.johnston@me.com>
2024-06-18 16:36:08 +0100

    Created names_zip.py

---
+++ intro_to_programming/loops/names_zip.py
@@ -0,0 +1,6 @@
+forenames = ['Ken', 'Lynn', 'Pat', 'Nancy']
+surnames = ['Camp', 'Blake', 'Flanagan', 'Short']
+
+zipped_names = zip(forenames, surnames)
+for forename, surname in zipped_names:
+    print(f'{forename} {surname}')
