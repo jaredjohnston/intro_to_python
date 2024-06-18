29194b1cec8514042c634a871ae7a872f670dd85
Jared Johnston <jared.johnston@me.com>
2024-06-18 13:25:28 +0100

    Modified names_for_loop.py

--- intro_to_programming/loops/names_for_loop.py
+++ intro_to_programming/loops/names_for_loop.py
@@ -2,7 +2,7 @@ names = ['Jared', 'Daniel', 'Eugene', 'Beatrice']
 upper_names = []
 
 for name in names:
-  upper_name = names[index].upper()
+  upper_name = name.upper()
   upper_names.append(upper_name)
 
 print(upper_names)
